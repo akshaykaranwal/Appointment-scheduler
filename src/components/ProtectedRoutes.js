356235c63e8b74c482197d270dbe333ae8a5bc2f
Akshay Karanwal <101737751+akshaykaranwal@users.noreply.github.com>
2023-09-12 21:49:46 +0530

    Add files via upload

---
+++ src/components/ProtectedRoutes.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { hideLoading, showLoading } from "../redux/features/alertSlice";
+import { setUser } from "../redux/features/userSlice";
+
+export default function ProtectedRoute({ children }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.user);
+
+  //get user
+  //eslint-disable-next-line
+  const getUser = async () => {
+    try {
+      dispatch(showLoading());
+      const res = await axios.post(
+        "/api/v1/user/getUserData",
+        { token: localStorage.getItem("token") },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(hideLoading());
+      if (await res.data.success) {
+        dispatch(setUser(await res.data.data));
+      } else {
+        localStorage.clear();
+        navigate("/login");
+      }
+    } catch (error) {
+      localStorage.clear();
+      dispatch(hideLoading());
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!user) {
+      getUser();
+    }
+  }, [user, getUser]);
+
+  if (localStorage.getItem("token")) {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+}
